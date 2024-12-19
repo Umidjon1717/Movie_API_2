f47c66abc9168075110380a99e216a457a6a892d
Umidjon1717 <umidizzatullayev1717@gmail.com>
2024-12-19 10:58:13 +0500

    swiper

--- src/components/movies/Movies.jsx
+++ src/components/movies/Movies.jsx
@@ -3,7 +3,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import { Pagination, Navigation } from "swiper/modules";
+import { Pagination, Navigation, Autoplay } from "swiper/modules";
 import MovieItem from "./MovieItem";
 import AdsItems from "./AdsItems";
 import { request } from "@/api";
@@ -11,31 +11,30 @@ import { request } from "@/api";
 const Movies = ({ data }) => {
   console.log(data);
 
- 
-
   return (
     <>
-
       <Swiper
-        modules={[Pagination, Navigation]}
+        modules={[Pagination, Navigation, Autoplay]}
         spaceBetween={20}
         slidesPerView={1}
         navigation={{
           nextEl: ".swiper-button-next",
           prevEl: ".swiper-button-prev",
         }}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+        }}
         className="container"
       >
         {data?.results?.map((movie) => (
           <SwiperSlide key={movie.id}>
             <AdsItems {...movie} />
-            
           </SwiperSlide>
         ))}
         <div className="swiper-button-next" style={{ color: "#ff0000" }}></div>
         <div className="swiper-button-prev" style={{ color: "#ff0000" }}></div>
       </Swiper>
-
       <Swiper
         modules={[Pagination, Navigation]}
         spaceBetween={20}
